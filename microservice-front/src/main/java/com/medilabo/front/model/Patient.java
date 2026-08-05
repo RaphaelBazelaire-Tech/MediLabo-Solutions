ffd19acc34924d43a6b70884f8bc90f08be206f9
Kyorisan <contact.kyorisan@gmail.com>
2026-08-05 10:15:18 +0200

    Microservice-front : Adding lombok to pom.xml and Patient model class

---
+++ microservice-front/src/main/java/com/medilabo/front/model/Patient.java
@@ -0,0 +1,17 @@
+package com.medilabo.front.model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class Patient {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String gender;
+    private String address;
+    private String phone;
+}
