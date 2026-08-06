66a0419541abc0146b65404e8f10577ee6585ff9
Kyorisan <contact.kyorisan@gmail.com>
2026-08-06 15:25:43 +0200

    Microservice-note : Adding Note model

---
+++ microservice-note/src/main/java/com/medilabo/note/model/Note.java
@@ -0,0 +1,33 @@
+package com.medilabo.note.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection =  "notes")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Note {
+
+    @Id
+    private String id;
+
+    @NotNull
+    private Integer patientId;
+
+    private String patient;
+
+    @NotBlank
+    private String note;
+
+    public Note(Integer patientId, String patient, String note) {
+        this.patientId = patientId;
+        this.patient = patient;
+        this.note = note;
+    }
+}
