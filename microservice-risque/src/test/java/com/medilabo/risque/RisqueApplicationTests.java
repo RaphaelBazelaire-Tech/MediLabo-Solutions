cf542f3fa563945563c8a2e202f9b8a727191caf
Kyorisan <contact.kyorisan@gmail.com>
2026-08-10 11:34:12 +0200

    Microservice-risque : Adding files

---
+++ microservice-risque/src/test/java/com/medilabo/risque/RisqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.risque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RisqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
