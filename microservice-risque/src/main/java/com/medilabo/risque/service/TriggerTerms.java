ee8936514ad5d42294b913eaa7cd91d37c7e6b62
Kyorisan <contact.kyorisan@gmail.com>
2026-08-10 13:46:24 +0200

    Microservice-risque : Adding TriggerTerms class

---
+++ microservice-risque/src/main/java/com/medilabo/risque/service/TriggerTerms.java
@@ -0,0 +1,23 @@
+package com.medilabo.risque.service;
+
+import java.util.List;
+
+public final class TriggerTerms {
+
+    private TriggerTerms() {}
+
+    public static final List<String> TERMS = List.of(
+            "hémoglobine a1c",
+            "microalbumine",
+            "taille",
+            "poids",
+            "fumeur",
+            "fumeuse",
+            "anormal",
+            "cholestérol",
+            "vertige",
+            "rechute",
+            "réaction",
+            "anticorps"
+    );
+}
